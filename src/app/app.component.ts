4c459b36919239fb76f1fa0d99df574f57ac3b46
bewithgun <ommore501@gmail.com>
2020-05-14 17:03:12 +0530

    Compllete assignment 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+default = 'advanced';
+OnSubmit(f)
+{
+  console.log("Email is",f.value.email);
+  console.log("Package is",f.value.suscribtion);
+  console.log("Password is",f.value.password);
+  f.reset();
+
+}
 
 }
