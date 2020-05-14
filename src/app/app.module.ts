2b9c0c095847d433b6dde0af57f1cdb440574b0a
bewithgun <ommore501@gmail.com>
2020-05-14 15:52:10 +0530

    Prepare for assignment 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 
-var routesOfApp: Routes =
-[
-  {path: 'user', component: UserComponent},
-  {path: 'server', component: ServerComponent},
-  {path: 'servers' , component: ServersComponent},
-  {path : 'users', component: UsersComponent},
-  {path: 'user/:id', component: UserComponent},
-  {path: 'servers/:id' , component: ServerComponent}
-]
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routesOfApp)
   ],
-  providers: [ServersService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
