af222e5ccc6708bebbf47b210e84dd00f11e828e
bewithgun <ommore501@gmail.com>
2020-05-17 19:55:49 +0530

    Prepare for assignment 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-formJS : FormGroup;
-
-  onSubmit()
-  {
-    console.log("My form",this.formJS.get('ProjectName').value);
-    console.log("My form",this.formJS.get('ProjectMail').value);
-    console.log("My form",this.formJS.get('ProjectStatus').value);
+export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  filteredStatus = '';
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
-
-  ngOnInit()
-  {
-    this.formJS = new FormGroup(
-      {
-        'ProjectName' : new FormControl(null, [Validators.required,this.forbiddenProjectNames], this.forbiddenProjectNamesOnServer)
-        ,'ProjectMail' : new FormControl('',[Validators.required,Validators.email]),
-        'ProjectStatus' : new FormControl(null,Validators.required)
-      }
-    );
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
   }
-  forbiddenProjectNames(control : FormControl): {[s: string]: boolean}
-  {
-    if(control.value === 'Test')
-      return {isNameForbidden: true};
-    else
-      return null;
-  }
-  forbiddenProjectNamesOnServer(control : FormControl): Promise<any> | Observer<any>
-  {
-    var promise = new Promise<any>(
-      (resolve, reject) =>
-      {
-        setTimeout(() =>
-        {
-         if(control.value === 'Test2')
-          resolve({isNameForbidden: true});
-         else
-          resolve(null);
-        },2000)
-      }
-    )
-    return promise;
- }
 }
