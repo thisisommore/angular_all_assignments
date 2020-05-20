832996b23dc38475911311f76150859ddb21f870
bewithgun <ommore501@gmail.com>
2020-05-20 19:36:46 +0530

    Prepare for small work given by instructor in lecture 262

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { reverse } from './reverse.pipe';
-import { filter } from './filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    reverse,
-    filter
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
